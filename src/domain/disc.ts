e8d3a5496e9512fd8b54b065351b5ab820137ea0
Imura-Yuto <yuto.imura1610@gmail.com>
2026-02-01 23:45:26 +0900

    12-69: ドメインモデルのメソッドの簡易実装

--- src/domain/disc.ts
+++ src/domain/disc.ts
@@ -1,7 +1,11 @@
-export const disc = {
+export const Disc = {
     Empty: 0,
     Dark: 1,
     Light: 2,
 } as const
 
-export type Disc = typeof disc[keyof typeof disc]
+export type Disc = typeof Disc[keyof typeof Disc]
+
+export function toDisc(value: number): Disc {
+    return value as Disc
+}
